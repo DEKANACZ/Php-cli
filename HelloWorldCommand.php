d89d63514e61a20cfac664d95945f9a7e07cf422
DEKANACZ <dekanac344a@gmail.com>
2022-12-28 14:57:46 +0100

    Current BitCoin price

--- HelloWorldCommand.php
+++ HelloWorldCommand.php
@@ -9,12 +9,28 @@ class HelloWorldCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('hello:world')
-            ->setDescription('Outputs \'Hello World\'');
+        $this->setName('btc:price')
+            ->setDescription('Outputs \'Bitcoin price\'');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello World BOUBOU');
+        $endpoint = "https://api.coinpaprika.com/v1/tickers";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $responseData = json_decode($response, true);
+
+        $bitcoinPrice = floatval($responseData[0]['quotes']['USD']['price']);
+        $bitcoinPrice = number_format($bitcoinPrice, 2);
+
+        $output->writeln("\033[0;45mThe current price of Bitcoin is \033[0m \033[0;33m $" . $bitcoinPrice." \033[0m");
+        return 0;
     }
 }
